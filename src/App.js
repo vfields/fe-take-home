f98096bb32ff20c21697783292e1630688fef0c4
Victoria Fields <victoriaashleyfields@gmail.com>
2023-01-03 15:01:56 -0700

    Add functional GET request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { useState, useEffect } from 'react';
 import Articles from './Articles/Articles.js'
 import './App.css';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.nytimes.com/svc/topstories/v2/home.json?api-key=ybisF2CGGDEAJBCUhCzPdJMIdJoBGGDo')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => setArticles(data.results))
+  }, [])
+
   return (
     <main>
       I am App
-      <Articles />
+      <Articles
+        articles={articles}
+      />
     </main>
   );
 }
