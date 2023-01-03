eb1a45667fc46a42b1e7d97c2a8348a3528f46cc
Victoria Fields <victoriaashleyfields@gmail.com>
2023-01-03 15:11:17 -0700

    Create unstyled article cards displaying correct info

--- src/ArticleCard/ArticleCard.js
+++ src/ArticleCard/ArticleCard.js
@@ -1,9 +1,15 @@
 import './ArticleCard.css';
 
-function ArticleCard() {
+function ArticleCard({ article }) {
+  const { title, byline, url } = article;
+  const imgUrl = article.multimedia[0].url;
+
   return (
     <article>
-      I am ArticleCard
+      <h3>{title}</h3>
+      <img src={imgUrl} />
+      <h4>{byline}</h4>
+      <p><a href={url}>Read this article</a></p>
     </article>
   );
 }
