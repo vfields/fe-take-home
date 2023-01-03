160bfbfb5d28513fa3b73872bcdbb65abf2be040
Victoria Fields <victoriaashleyfields@gmail.com>
2023-01-03 15:02:32 -0700

    Add articles display info for unique article cards

--- src/Articles/Articles.js
+++ src/Articles/Articles.js
@@ -1,11 +1,21 @@
 import ArticleCard from '../ArticleCard/ArticleCard.js'
 import './Articles.css';
 
-function Articles() {
+function Articles({ articles }) {
+  const articleDisplay = articles.map(article => {
+    const uniqueKey = `${article.title}_${article.published_date}`
+    return (
+      <ArticleCard
+        key={uniqueKey}
+        article={article}
+      />
+    )
+  })
+
   return (
     <section>
       I am Articles
-      <ArticleCard />
+      {articleDisplay}
     </section>
   );
 }
