9ac22b359870a352b37e4feb9d332676294fd601
Victoria Fields <victoriaashleyfields@gmail.com>
2023-01-04 09:54:56 -0700

    bug fix for detailed article display

--- src/ArticleDetails/ArticleDetails.js
+++ src/ArticleDetails/ArticleDetails.js
@@ -1,9 +1,20 @@
 import './ArticleDetails.css';
 
 function ArticleDetails({ article }) {
+  const displayArticle = (articleInfo) => {
+    return (
+      <article>
+        {articleInfo.title}
+        {articleInfo.byline}
+      </article>
+    )
+  }
+
+  const display = article ? displayArticle(article) : "loading..."
+
   return (
     <section>
-      I am ArticleDetails, here are some details: {article.title}
+      I am ArticleDetails, here are some details: {display}
     </section>
   );
 }
