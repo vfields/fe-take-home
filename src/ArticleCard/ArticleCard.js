433a86fc1ea897e5586332a0e0fcb196047743ea
Victoria Fields <victoriaashleyfields@gmail.com>
2023-01-03 17:39:18 -0700

    Add alt text to article imgs

--- src/ArticleCard/ArticleCard.js
+++ src/ArticleCard/ArticleCard.js
@@ -3,11 +3,12 @@ import './ArticleCard.css';
 function ArticleCard({ article }) {
   const { title, byline, url } = article;
   const imgUrl = article.multimedia[0].url;
+  const altText = article.multimedia[0].caption;
 
   return (
     <article className="article-card">
       <h3>{title}</h3>
-      <img src={imgUrl} />
+      <img src={imgUrl} alt={altText} />
       <h4>{byline}</h4>
       <p>Learn more | <a href={url}>Read this article</a></p>
     </article>
